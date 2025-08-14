840ac99c036f9e829c80cd42f513f617540672e9
falguni-mondal <career.falguni@gmail.com>
2025-08-14 20:11:24 +0530

    Real time code updation

--- src/components/Dev/CodeEditor.jsx
+++ src/components/Dev/CodeEditor.jsx
@@ -8,18 +8,24 @@ const CodeEditor = () => {
 
     const [snippet, setSnippet] = useState(null);
 
-    const {id} = useParams();
+    const { id } = useParams();
 
     const snippetFinder = async () => {
-        const res = await axios.post(`${baseUrl}/api/snippet/find`, {id}, {withCredentials:true});
-        if(res.data){
-            setSnippet(res.data);
+        const res = await axios.post(`${baseUrl}/api/snippet/find`, { id }, { withCredentials: true });
+        if (res?.data) {
+            setSnippet(res?.data);
         }
     }
 
-    const changesHandler = (value) => {
-        console.log(value);
+    let timer = null;
+
+    const changeHandler = (value) => {
         
+        clearTimeout(timer);
+        timer = setTimeout(async () => {
+            const res = await axios.patch(`${baseUrl}/api/snippet/code/update`, {id, code: value}, {withCredentials: true});
+        }, 1500)
+
     }
 
     useEffect(() => {
@@ -28,13 +34,23 @@ const CodeEditor = () => {
 
     return (
         snippet &&
-        <Editor
-            height="90vh"
-            theme="vs-dark"
-            language={snippet.language}
-            value={snippet.content}
-            onChange={(newValue) => changesHandler(newValue)}
-        />
+        <div className="editor-page-wrapper w-full flex">
+            <div className="leftnav w-[16%] pt-1 pl-10">
+                <h2 className='text-[0.9rem] font-medium text-zinc-300'>Collaborators</h2>
+                <div className="collaborators-list">
+
+                </div>
+            </div>
+            <div className="editor-page w-[84%] pt-2">
+                <Editor
+                    height="88vh"
+                    theme="hc-black"
+                    language={snippet.language}
+                    value={snippet.content}
+                    onChange={(newValue) => changeHandler(newValue)}
+                />
+            </div>
+        </div>
     )
 }
 
