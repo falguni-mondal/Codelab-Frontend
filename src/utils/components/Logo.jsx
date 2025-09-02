60f52efef665845c9b3b228d2cac105f45f1caf5
falguni-mondal <career.falguni@gmail.com>
2025-09-02 22:07:18 +0530

    Change of name

--- src/utils/components/Logo.jsx
+++ src/utils/components/Logo.jsx
@@ -2,7 +2,7 @@ import { ImLab } from "react-icons/im";
 
 const Logo = () => {
   return (
-    <div className="logo flex items-center text-[1.1rem] font-medium">CodeLab<ImLab className='text-[#00823F]' /></div >
+    <div className="logo flex items-center text-[1.1rem] font-medium">Synctax<ImLab className='text-[#00823F]' /></div >
   )
 }
 
